8bfc251c5f2f405e6eff04c47cea2ac927e542aa
Francisco Knebel <franciscopaivaknebel@gmail.com>
2017-07-04 00:30:32 -0300

    create2: Tupla de FIM vira MAP.

--- src/files.c
+++ src/files.c
@@ -2,9 +2,8 @@
   INF01142 - Sistemas Operacionais I
   T2FS - 2017/1
 
-  Módulo desenvolvido por Francisco Knebel
-  Funções:
-    initTupla, initFileRegister, createRecord, addRecord
+  Douglas Lazaro
+  Francisco Knebel
 */
 
 #include "libs.h"
@@ -20,10 +19,10 @@ struct t2fs_4tupla initTupla(DWORD atributeType, DWORD VBN, DWORD LBN, DWORD num
   return tupla;
 }
 
-int initFileRegister(int registerIndex, int VBN) {
+int initFileRegister(int registerIndex, int LBN) {
   struct t2fs_4tupla tupla, tuplaFim;
 
-  tupla     = initTupla(REGISTER_MAP, VBN, 0, 1);
+  tupla     = initTupla(REGISTER_MAP, 0, LBN, 1);
   tuplaFim  = initTupla(REGISTER_FIM, 0, 0, 0);
 
   REGISTER_T reg;
@@ -46,17 +45,29 @@ struct t2fs_record createRecord(char* pathname) {
   memset(&newFile, 0, sizeof(newFile));
 
   newFile.TypeVal = TYPEVAL_REGULAR;
-  strcpy(newFile.name, parsedPath[parseCount]); //name é o último elemento parseado: /directory/directory/file1 -> name = file1
+  strcpy(newFile.name, parsedPath[parseCount]); // name é o último elemento parseado: /directory/directory/file1 -> name = file1
   newFile.blocksFileSize = 1;
   newFile.bytesFileSize = 0;
 
   /* Encontrar registro do MFT livre, escrever registro e associar ao arquivo. */
-  int fileVBN = searchBitmap2(BM_LIVRE); // Encontra bloco de dados para o arquivo
-  setBitmap2(fileVBN, BM_OCUPADO);
+  int check;
+
+  int fileLBN = searchBitmap2(BM_LIVRE); // Encontra bloco de dados para o arquivo
+  check = setBitmap2(fileLBN, BM_OCUPADO);
+  /*
+  if (check < 0) {
+    return BM_ERROR;
+  }
+  */
 
   int registro = searchMFT(MFT_BM_LIVRE);
-  initFileRegister(registro, fileVBN); // Inicializa registro no MFT do arquivo
-  setMFT(registro, MFT_BM_OCUPADO);    // seta registro como ocupado no bitmap.
+  initFileRegister(registro, fileLBN); // Inicializa registro no MFT do arquivo
+  check = setMFT(registro, MFT_BM_OCUPADO);    // seta registro como ocupado no bitmap.
+  /*
+  if (check < 0) {
+    return MFT_BM_ERROR;
+  }
+  */
 
   newFile.MFTNumber = registro; // Associa arquivo ao registro criado
 
@@ -79,7 +90,7 @@ int addToDirectory(DWORD directoryMFTNumber, struct t2fs_record record) {
   BLOCK_T blockBuffer;
   blockBuffer.at = malloc(sizeof(unsigned char) * constants.BLOCK_SIZE);
 
-  int i = 0, amountOfBlocksRead = 0, foundSpaceToAdd = FALSE, block, allocated;
+  int i = 0, amountOfBlocksRead = 0, foundSpaceToAdd = FALSE, block, allocated, fileBlocksCounter = 0;
   while (i < constants.MAX_TUPLAS_REGISTER && foundSpaceToAdd != TRUE) {
     switch (tuplas[i].atributeType) {
       case REGISTER_MAP:
@@ -107,6 +118,9 @@ int addToDirectory(DWORD directoryMFTNumber, struct t2fs_record record) {
           }
         }
 
+        fileBlocksCounter += amountOfBlocksRead;
+        amountOfBlocksRead = 0;
+
         if(foundSpaceToAdd != TRUE) {
           i++;
         }
@@ -132,23 +146,45 @@ int addToDirectory(DWORD directoryMFTNumber, struct t2fs_record record) {
           if(writeRecord(block, 0, record) == FALSE) {
             return RECORD_WRITE_ERROR;
           };
-        } else {
+        } else if(i == constants.MAX_TUPLAS_REGISTER - 1) { // tupla atual no final do registro?
           /*
-            Verificar se i+1 == máximo de tuplas (significa que a tupla fica no final do registro)
             Se sim,
               tupla[i] vira REGISTER_ADITIONAL
               encontrar registro livre com o Bitmap MFT.
               passar registro para tupla[i]
               setar registro como ocupado.
 
-              criar novo registro, verificando blocos livres e setando ocupação.
+              criar novo registro MAP, verificando blocos livres e setando ocupação.
               no novo bloco da primeira nova tupla, adicionar o record.
-            Se não
-              setar tupla[i+1] como REGISTER_FIM
-              tupla[i] vira REGISTER_MAP
-              encontra bloco de dados livre e seta como ocupado.
-              adiciona o record no novo bloco
           */
+        } else {
+          /* Não está no final, logo a tupla atual vira REGISTRO_MAP para novos blocos do arquivo. */
+
+          /* ATUALIZAÇÃO DO REGISTRO */
+          REGISTER_T reg;
+          readRegister(registerIndex, &reg);
+
+          int newLBN = searchBitmap2(BM_LIVRE); // Encontra bloco de dados para o arquivo
+          int check = setBitmap2(newLBN, BM_OCUPADO);
+          if (check < 0) {
+            return BM_ERROR;
+          }
+
+          // Tupla atual vira um MAP, para os novos blocos do arquivo.
+          tuplas[i] = initTupla(REGISTER_MAP, fileBlocksCounter, newLBN, 1);
+          writeTupla(reg.at, &tuplas[i], i);
+
+          // Tupla seguinte vira o novo fim do registro.
+          tuplas[i+1] = initTupla(REGISTER_FIM, 0, 0, 0);
+          writeTupla(reg.at, &tuplas[i+1], i+1);
+
+          // Atualização do registro com novos valores.
+          writeRegister(registerIndex, &reg);
+
+          /* ATUALIZAÇÃO DO BLOCO DE DIRETÓRIO */
+          if(writeRecord(tuplas[i].logicalBlockNumber, 0, record) == FALSE) {
+            return RECORD_WRITE_ERROR;
+          };
         }
 
         break;
@@ -207,6 +243,52 @@ int addRecordToDirectory(struct t2fs_record record, char * filename) {
       return_value = addToDirectory(directory.MFTNumber, record);
 
       break;
+  }
+
+  return return_value;
+}
+
+int openFile(char* filename) {
+  struct t2fs_record file;
+  int return_value = lookup(filename, &file);
+
+  switch (return_value) {
+    case REGISTER_READ_ERROR:
+      printf("Erro crítico na leitura de um registro no lookup.\n");
+      break;
+    case FIND_REGISTER_ADITIONAL:
+      printf("Erro! Valor de retorno de lookup nunca deve ser FIND_REGISTER_ADITIONAL.\n");
+      break;
+    case PARSED_PATH_ERROR:
+      printf("Path '%s' inválida.\n", filename);
+      break;
+    default:
+      if(return_value >= 0) {
+        if (isFreeLDAA() == TRUE) {
+          return_value = insertLDAA(file);
+        } else {
+          return_value = FILE_LIMIT_REACHED;
+        }
+      } else {
+        return_value = FILE_NOT_FOUND;
+      }
+  }
+
+  return return_value;
+}
+
+int closeFile(int handle, BYTE TypeVal) {
+  int return_value = -1;
+  struct descritor result;
+
+  if(searchLDAA(handle, TypeVal, &result) == TRUE) {
+    if (removeLDAA(handle) == TRUE) {
+      return_value = 0;
+    } else {
+      return_value = ERRO_REMOVE_LDAA;
+    }
+  } else {
+    return_value = NOT_FOUND_LDAA;
   }
 
   return return_value;
