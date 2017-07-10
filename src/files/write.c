f43b2b3d44118fb55d27e53e5ca9a666c9797476
Francisco Knebel <franciscopaivaknebel@gmail.com>
2017-07-10 02:55:17 -0300

    Write inicial, dentro do primeiro bloco.

--- src/files/write.c
+++ src/files/write.c
@@ -9,5 +9,110 @@
 #include "libs.h"
 
 int writeFile(int handle, struct descritor descritor, char * buffer, unsigned int size) {
-  return -1;
+  int return_value = -1;
+  int registerIndex = descritor.record.MFTNumber;
+
+  REGISTER_T reg;
+  if(readRegister(registerIndex, &reg) != TRUE) {
+    return FALSE;
+  }
+
+  struct t2fs_4tupla *tuplas = malloc(constants.MAX_TUPLAS_REGISTER * sizeof(struct t2fs_4tupla));
+  parseRegister(reg.at, tuplas);
+
+  BLOCK_T blockBuffer;
+  blockBuffer.at = malloc(sizeof(unsigned char) * constants.BLOCK_SIZE);
+
+  unsigned int i = 0, block;
+  unsigned int bytesWritten = 0, bytesLeft = size;
+  unsigned int amountOfBlocksRead = 0;
+
+  // Achar tupla, bloco e offset inicial, de acordo com currentPointer.
+  unsigned int initialBlock = descritor.currentPointer / constants.BLOCK_SIZE;
+  unsigned int initialOffset = descritor.currentPointer % constants.BLOCK_SIZE;
+  i = findOffsetTupla(tuplas, initialBlock, &reg);
+
+  while (i < constants.MAX_TUPLAS_REGISTER && bytesLeft > (unsigned int) 0) {
+    switch(tuplas[i].atributeType) {
+      case REGISTER_MAP:
+        amountOfBlocksRead = initialBlock;
+        initialBlock = 0;
+        while(amountOfBlocksRead < tuplas[i].numberOfContiguosBlocks && bytesLeft > (unsigned int) 0) {
+          block = tuplas[i].logicalBlockNumber + amountOfBlocksRead;
+          amountOfBlocksRead++;
+
+          if(readBlock(block, &blockBuffer) == FALSE) {
+            return FALSE;
+          };
+
+          if(bytesLeft <= constants.BLOCK_SIZE) {
+            // Escreve dados no buffer do bloco, e depois escreve no disco.
+            memcpy(blockBuffer.at + initialOffset, &buffer[bytesWritten], bytesLeft);
+            writeBlock(block, &blockBuffer);
+
+            // Chegou no final da escrita
+            bytesWritten += bytesLeft;
+            bytesLeft = 0;
+
+            // Atualiza descritor e record na LDAA
+
+            // Se o ponteiro estiver antes do final, não deve incrementar o tamanho do arquivo, e sim apenas sobreescrever
+            if(descritor.currentPointer < descritor.record.bytesFileSize) {
+              unsigned int offBW = descritor.record.bytesFileSize - descritor.currentPointer;
+              if(offBW < bytesWritten) {
+                descritor.record.bytesFileSize += bytesWritten - offBW;
+              }
+            } else {
+              descritor.record.bytesFileSize += bytesWritten;
+            }
+
+            descritor.currentPointer += bytesWritten;
+            descritor.record.blocksFileSize = (descritor.record.bytesFileSize / constants.BLOCK_SIZE) + 1;
+            updateLDAA(handle, TYPEVAL_REGULAR, descritor);
+
+            // Atualiza record no diretório
+            addRecordToDirectory(descritor.record, descritor.name, TRUE);
+
+            return_value = bytesWritten;
+          } else {
+            // Escreve dados no buffer do bloco, e depois escreve no disco.
+            memcpy(blockBuffer.at + initialOffset, &buffer[bytesWritten], constants.BLOCK_SIZE - initialOffset);
+            //writeBlock(block, &blockBuffer);
+
+
+          }
+
+
+          initialOffset = 0;
+        }
+
+        if(bytesLeft > 0) {
+          i++;
+        }
+
+        break;
+      case REGISTER_ADITIONAL:
+        // Ler novo registro e recomeçar a leitura.
+        registerIndex = tuplas[i].virtualBlockNumber;
+
+        if(readRegister(registerIndex, &reg) != TRUE) {
+          return FALSE;
+        }
+        free(tuplas);
+        tuplas = malloc(constants.MAX_TUPLAS_REGISTER * sizeof(struct t2fs_4tupla));
+
+        parseRegister(reg.at, tuplas);
+        i = 0; // reset i para 0, começar a ler tuplas novamente
+
+      case REGISTER_FIM:
+        bytesLeft = 0;
+        return_value = bytesWritten;
+        break;
+      default:
+        return_value = bytesWritten;
+        break;
+    }
+  }
+
+  return return_value;
 }
--- teste/api/write.c
+++ teste/api/write.c
@@ -7,7 +7,160 @@
   Desenvolvido por Francisco Knebel
 */
 
-int main(int argc, char const *argv[]) {
-  /* code */
+#include <time.h>
+
+#include "libs.h"
+
+#define IDENTIFY_SIZE 80
+
+int read(int handle, int size, char* buffer) {
+  if(handle < 0) {
+    printf("%d: Erro abrindo arquivo.\n", handle);
+    return -1;
+  }
+
+  int check = read2(handle, buffer, size);
+  if(check < 0) {
+    printf("%d: Erro lendo arquivo.\n", check);
+    return -1;
+  }
+
+  return check;
+}
+
+int write(int handle, int size, char* buffer) {
+  if(handle < 0) {
+    printf("%d: Erro abrindo arquivo.\n", handle);
+    return -1;
+  }
+
+  int check = write2(handle, buffer, size);
+  if(check < 0) {
+    printf("%d: Erro escrevendo arquivo.\n", check);
+    return -1;
+  }
+
+  return check;
+}
+
+char *randstring(int length) {
+    char *string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+    size_t stringLen = 26*2+10+7;
+    char *randomString;
+
+    randomString = malloc(sizeof(char) * (length +1));
+
+    if (!randomString) {
+        return (char*)0;
+    }
+
+    unsigned int key = 0;
+    int n;
+    for (n = 0;n < length;n++) {
+        key = rand() % stringLen;
+        randomString[n] = string[key];
+    }
+
+    randomString[length] = '\0';
+
+    return randomString;
+}
+
+void readAndPrint(int handle, char* path, int size) {
+  int offset = 0, check;
+  char *newBuffer = malloc(sizeof(char) * size);
+  strcpy(newBuffer, "");
+  seek2(handle, 0);
+  do {
+    check = read(handle, 3, &newBuffer[offset]);
+    if(check > 0) {
+      offset += check;
+    }
+  } while(check != 0);
+
+  newBuffer[offset] = '\0';
+  printf("\nImprimindo arquivo '%s', de '%d' bytes:\n", path, offset);
+  printf("%s", newBuffer);
+}
+
+void test_openFileAndWrite(char* path) {
+  int handle = open2(path);
+  seek2(handle, -1); // Ponteiro para o final do arquivo
+
+  write(handle, 10, randstring(10));
+  write(handle, 10, randstring(10));
+  write(handle, 10, randstring(10));
+
+  readAndPrint(handle, path, 30);
+
+  close2(handle);
+}
+
+void test_writeRoot(char* path, int size, char* buffer) {
+  int check;
+
+  int handle = open2(path);
+  seek2(handle, -1);
+
+  check = write(handle, strlen(buffer), buffer);
+
+  char newBuffer[size];
+  seek2(handle, 0);
+  check = read(handle, size, newBuffer);
+  if(check > 0) {
+    printf("\nImprimindo arquivo '%s', de '%d' bytes:\n", path, check);
+    puts(newBuffer);
+  }
+
+  close2(handle);
+}
+
+void test_writeBlock(char* path) {
+  int handle = open2(path);
+
+  int i = 0, loops = 16, bytesPerWrite = 64;
+  int size = loops * bytesPerWrite;
+  for (i = 0; i < loops; i++) {
+    write(handle, bytesPerWrite, randstring(bytesPerWrite));
+  }
+
+  readAndPrint(handle, path, size);
+
+  printf("\n\n");
+  BLOCK_T block;
+  block.at = malloc(sizeof(char) * constants.BLOCK_SIZE);
+
+  readBlock(2052, &block);
+  printBlock(block.at);
+
+  close2(handle);
+}
+
+void test_write2() {
+  /* WRITE FILE */
+  printf("\n--- WRITE2 ---\n");
+  char buffer[10];
+  strcpy(buffer, "0123456789");
+
+  //test_writeRoot("/file1", 100, buffer);
+
+  //test_openFileAndWrite("/file2");
+
+  test_writeBlock("/file2");
+
+  printf("\n--- ENCERRANDO WRITE2 ---\n");
+  return;
+}
+
+int main() {
+  char identify[IDENTIFY_SIZE];
+
+  identify2(identify, IDENTIFY_SIZE);
+  printf("%s\n\n", identify);
+  srand(time(NULL));
+  initConfig();
+
+  test_write2();
+
   return 0;
 }
